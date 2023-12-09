725a5c8ee73b9869d99926bbe0502d9bec625af1
sszuev <sss.zuev@gmail.com>
2023-12-09 22:36:40 +0300

    jena-core-ont-tests: more `OntModelClassesTest`s

--- src/test/java/com/gitlab/sszuev/jena/ont/OntModelClassesTest.java
+++ src/test/java/com/gitlab/sszuev/jena/ont/OntModelClassesTest.java
@@ -31,10 +31,12 @@ public class OntModelClassesTest {
     public void testListClasses1a(TestSpec spec) {
         OntModel m = ModelFactory.createOntologyModel(spec.inst, null);
         IOTestUtils.readResourceModel(m, "/jena/list-syntax-categories-test.rdf");
-        Assertions.assertEquals(
-                Set.of("Y0", "Z", "B", "D", "Y1", "X0", "C", "E", "X1", "A", "null"),
-                m.listClasses().mapWith(it -> it.isAnon() ? "null" : it.getLocalName()).toSet()
-        );
+        List<String> actual = Iter.asStream(m.listClasses())
+                .map(it -> it.isAnon() ? "null" : it.getLocalName())
+                .sorted()
+                .collect(Collectors.toList());
+        List<String> expected = List.of("A", "B", "C", "D", "E", "X0", "X1", "Y0", "Y1", "Z", "null");
+        Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest
@@ -149,5 +151,118 @@ public class OntModelClassesTest {
         }
         Assertions.assertEquals(expectedContainsAll, actual.containsAll(probes));
         Assertions.assertEquals(expectedClassCount, actual.size());
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {
+            "OWL_MEM",
+            "OWL_MEM_RDFS_INF",
+            "OWL_MEM_TRANS_INF",
+            "OWL_DL_MEM",
+            "OWL_DL_MEM_RDFS_INF",
+            "OWL_DL_MEM_TRANS_INF",
+            "OWL_LITE_MEM",
+            "OWL_LITE_MEM_RDFS_INF",
+            "OWL_LITE_MEM_TRANS_INF",
+    })
+    public void testListClasses3a(TestSpec spec) {
+        OntModel m = ModelFactory.createOntologyModel(spec.inst);
+        IOTestUtils.readResourceModel(m, "/jena/list-syntax-categories-test-comps.xml");
+        List<String> expected = List.of(
+                "eg:Bundle", "eg:Computer", "eg:GameBundle", "eg:GamingComputer", "eg:GraphicsCard", "eg:MotherBoard",
+                "null", "null", "null"
+        );
+        List<String> actual = Iter.asStream(m.listClasses())
+                .map(it -> it.isAnon() ? "null" : m.shortForm(it.getURI()))
+                .sorted()
+                .collect(Collectors.toList());
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {
+            "OWL_MEM_RULE_INF",
+            "OWL_MEM_MINI_RULE_INF",
+            "OWL_DL_MEM_RULE_INF",
+            "OWL_LITE_MEM_RULES_INF",
+    })
+    public void testListClasses3b(TestSpec spec) {
+        OntModel m = ModelFactory.createOntologyModel(spec.inst);
+        IOTestUtils.readResourceModel(m, "/jena/list-syntax-categories-test-comps.xml");
+        List<String> expected = List.of(
+                "eg:Bundle", "eg:Computer", "eg:GameBundle", "eg:GamingComputer", "eg:GraphicsCard", "eg:MotherBoard",
+                "null", "null", "null", "null", "null",
+                "owl:Class", "owl:Nothing", "owl:Ontology", "owl:Property", "owl:Restriction", "owl:Thing",
+                "rdf:List", "rdf:Property", "rdf:Statement",
+                "rdfs:Class", "rdfs:Literal", "rdfs:Resource",
+                "xsd:boolean", "xsd:byte", "xsd:date", "xsd:dateTime", "xsd:decimal", "xsd:duration", "xsd:float", "xsd:int",
+                "xsd:integer", "xsd:long", "xsd:nonNegativeInteger", "xsd:nonPositiveInteger", "xsd:short", "xsd:string",
+                "xsd:time", "xsd:unsignedByte", "xsd:unsignedInt", "xsd:unsignedLong", "xsd:unsignedShort"
+        );
+        List<String> actual = Iter.asStream(m.listClasses())
+                .map(it -> it.isAnon() ? "null" : m.shortForm(it.getURI()))
+                .sorted()
+                .collect(Collectors.toList());
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {
+            "RDFS_MEM",
+            "RDFS_MEM_TRANS_INF",
+    })
+    public void testListClasses3c(TestSpec spec) {
+        OntModel m = ModelFactory.createOntologyModel(spec.inst);
+        IOTestUtils.readResourceModel(m, "/jena/list-syntax-categories-test-comps.xml");
+        List<String> expected = List.of();
+        List<String> actual = Iter.asStream(m.listClasses())
+                .map(it -> it.isAnon() ? "null" : m.shortForm(it.getURI()))
+                .sorted()
+                .collect(Collectors.toList());
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {
+            "RDFS_MEM_RDFS_INF",
+    })
+    public void testListClasses3d(TestSpec spec) {
+        OntModel m = ModelFactory.createOntologyModel(spec.inst);
+        IOTestUtils.readResourceModel(m, "/jena/list-syntax-categories-test-comps.xml");
+        List<String> expected = List.of(
+                "eg:Bundle", "eg:Computer", "eg:GameBundle", "eg:GraphicsCard", "eg:MotherBoard",
+                "null",
+                "owl:Class", "owl:ObjectProperty", "owl:Restriction", "owl:TransitiveProperty",
+                "rdf:Alt", "rdf:Bag", "rdf:List", "rdf:Property", "rdf:Seq", "rdf:Statement", "rdf:XMLLiteral",
+                "rdfs:Class", "rdfs:Container", "rdfs:ContainerMembershipProperty", "rdfs:Datatype", "rdfs:Literal", "rdfs:Resource"
+        );
+        List<String> actual = Iter.asStream(m.listClasses())
+                .map(it -> it.isAnon() ? "null" : m.shortForm(it.getURI()))
+                .sorted()
+                .collect(Collectors.toList());
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @EnumSource(names = {
+            "OWL_MEM_MICRO_RULE_INF",
+    })
+    public void testListClasses3e(TestSpec spec) {
+        OntModel m = ModelFactory.createOntologyModel(spec.inst);
+        IOTestUtils.readResourceModel(m, "/jena/list-syntax-categories-test-comps.xml");
+        List<String> expected = List.of(
+                "eg:Bundle", "eg:Computer", "eg:GameBundle", "eg:GamingComputer", "eg:GraphicsCard", "eg:MotherBoard",
+                "null", "null", "null", "null", "null", "owl:Class",
+                "owl:DatatypeProperty", "owl:FunctionalProperty", "owl:InverseFunctionalProperty", "owl:Nothing",
+                "owl:ObjectProperty", "owl:Ontology", "owl:OntologyProperty", "owl:Property",
+                "owl:Restriction", "owl:SymmetricProperty", "owl:Thing", "owl:TransitiveProperty",
+                "rdf:Alt", "rdf:Bag", "rdf:List", "rdf:Property", "rdf:Seq", "rdf:Statement",
+                "rdfs:Class", "rdfs:Container", "rdfs:ContainerMembershipProperty", "rdfs:Datatype", "rdfs:Literal", "rdfs:Resource"
+        );
+        List<String> actual = Iter.asStream(m.listClasses())
+                .map(it -> it.isAnon() ? "null" : m.shortForm(it.getURI()))
+                .sorted()
+                .collect(Collectors.toList());
+        Assertions.assertEquals(expected, actual);
     }
 }
